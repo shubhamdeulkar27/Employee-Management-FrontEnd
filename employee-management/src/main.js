30b62f7c7b2a9a7cbf458068ad9e488b9b162c15
shubham <shubhamdeulkar27@gmail.com>
2020-09-09 00:00:12 +0530

    Added: Axios Services

--- employee-management/src/main.js
+++ employee-management/src/main.js
@@ -5,17 +5,19 @@ import "vue-material/dist/vue-material.min.css";
 import "vue-material/dist/theme/default.css";
 import VueRouter from "vue-router";
 import Routes from "./routes.js";
+import axios from "axios";
 
+axios.defaults.baseURL = "https://localhost:44315/api/employee";
 Vue.use(VueMaterial);
 Vue.use(VueRouter);
 
 const router = new VueRouter({
   mode: "history",
-  routes: Routes
+  routes: Routes,
 });
 
 new Vue({
   el: "#app",
-  render: h => h(App),
-  router: router
+  render: (h) => h(App),
+  router: router,
 });
