a2ccd8b5dbff9a8d06ff30355bdc31df8b0db1be
shubham <shubhamdeulkar27@gmail.com>
2020-09-09 10:34:20 +0530

    Added Vuelidate Validations On Login Component

--- employee-management/src/components/Login.vue
+++ employee-management/src/components/Login.vue
@@ -4,19 +4,42 @@
       <md-icon>login</md-icon>
       <div id="container-title">Log in</div>
       <div id="form-container">
-        <form>
-          <md-field>
-            <label>Username</label>
-            <md-input v-model="form.UserName"></md-input>
+        <form novalidate class="md-layout" @submit.prevent="validateUser">
+          <md-field :class="getValidationClass('UserName')">
+            <label for="UserName">Username</label>
+            <md-input
+              name="UserName"
+              id="UserName"
+              v-model="form.UserName"
+              :disabled="sending"
+            ></md-input>
             <span class="md-helper-text"
               >You can use letters,numbers,period and @.</span
             >
+            <span class="md-error" v-if="!$v.form.UserName.required"
+              >The first name is required</span
+            >
+            <span class="md-error" v-else-if="!$v.form.UserName.minlength"
+              >Invalid first name</span
+            >
           </md-field>
-          <md-field>
-            <label>Password</label>
-            <md-input v-model="form.Password" type="password"></md-input>
+          <md-field :class="getValidationClass('Password')">
+            <label for="Password">Password</label>
+            <md-input
+              name="Password"
+              id="Password"
+              v-model="form.Password"
+              type="password"
+              :disabled="sending"
+            ></md-input>
             <span class="md-helper-text"
               >Should Be Alphanumeric and At least One special character.</span
+            >
+            <span class="md-error" v-if="!$v.form.Password.required"
+              >The first name is required</span
+            >
+            <span class="md-error" v-else-if="!$v.form.Password.minlength"
+              >Invalid first name</span
             >
           </md-field>
 
@@ -24,7 +47,10 @@
             <md-button class="md-dense md-primary" href="/register"
               >Create Account</md-button
             >
-            <md-button @click="login" class="md-raised md-primary"
+            <md-button
+              type="submit"
+              class="md-raised md-primary"
+              :disabled="sending"
               >Log In</md-button
             >
           </md-card-actions>
@@ -47,14 +73,19 @@
       md-persistent
     >
       <span>Login Successful!</span>
-      <md-button class="md-primary" @click="isLogin = false">Ok</md-button>
+      <md-button type="submit" class="md-primary" :disabled="sending"
+        >Ok</md-button
+      >
     </md-snackbar>
   </div>
 </template>
 <script>
 import service from "../services/axios-service.js";
+import { validationMixin } from "vuelidate";
+import { required, minLength, maxLength } from "vuelidate/lib/validators";
 export default {
   name: "Login",
+  mixins: [validationMixin],
   data() {
     return {
       form: {
@@ -64,10 +95,24 @@ export default {
       isLogin: false,
       invalidCredentials: false,
       position: "center",
+      sending: false,
     };
+  },
+  validations: {
+    form: {
+      UserName: {
+        required,
+        minLength: minLength(6),
+      },
+      Password: {
+        required,
+        minLength: minLength(6),
+      },
+    },
   },
   methods: {
     login() {
+      this.sending = true;
       let user = {
         UserName: this.form.UserName,
         Password: this.form.Password,
@@ -78,11 +123,36 @@ export default {
           this.isLogin = true;
           console.log(this.isLogin);
         })
+        .then(() => {
+          this.sending = false;
+          this.clearForm();
+        })
         .catch((error) => {
-          this.invalidCredentials = true;
-          this.form.UserName = null;
-          this.form.Password = null;
+          if (error.response.status == "404") {
+            this.invalidCredentials = true;
+          }
         });
+    },
+    getValidationClass(fieldName) {
+      const field = this.$v.form[fieldName];
+
+      if (field) {
+        return {
+          "md-invalid": field.$invalid && field.$dirty,
+        };
+      }
+    },
+    validateUser() {
+      this.$v.$touch();
+
+      if (!this.$v.$invalid) {
+        this.login();
+      }
+    },
+    clearForm() {
+      this.$v.$reset();
+      this.form.UserName = null;
+      this.form.Password = null;
     },
   },
 };
